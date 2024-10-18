2b9926be132dce19cf7f893561c475b26d3854c5
Isaac Freires Santos <isaacfreires@gmail.com>
2024-10-18 17:32:06 -0300

    ✨ feat: Adicionado gráfico de barras para ficar de acordo com filtros

--- candidatos.py
+++ candidatos.py
@@ -13,28 +13,43 @@ if base_csv != None:
     st.header("Prévia dos Dados")
     base
 
-st.sidebar.header("Filtros")
+    st.sidebar.header("Filtros")
 
-unidade_eleitoral = base.groupby('NM_UE')
+    unidade_eleitoral = base['NM_UE'].unique()
 
-st.sidebar.selectbox(
-    "Selecione uma unidade eleitoral(NM_UE)",
-    unidade_eleitoral,
-    index=None
-)
+    unidade_selecionada = st.sidebar.selectbox(
+        "Selecione uma unidade eleitoral(NM_UE)",
+        unidade_eleitoral,
+        index=None,
+        placeholder="Selecione uma unidade eleitoral"
+    )
 
-cargo = base.groupby('DS_CARGO')
+    cargo = base['DS_CARGO'].unique()
 
-st.sidebar.selectbox(
-    "Selecione um Cargo (DS_CARGO)",
-    cargo,
-    index=None
-)
+    cargo_selecionado = st.sidebar.selectbox(
+        "Selecione um Cargo (DS_CARGO)",
+        cargo,
+        index=None,
+        placeholder="Selecione um cargo"
+    )
 
-st.header("Distribuição do Grau de Instrução")
 
-st.write("Distribuição por Grau de Instrução")
+    # st.write(base_filtrada)
 
-grau_instrucao = base.groupby('DS_GRAU_INSTRUCAO').agg({'count': 'sum'}).reset_index()
+    st.header("Distribuição do Grau de Instrução")
 
-st.bar_chart(grau_instrucao, x='DS_GRAU_INSTRUCAO', y='count')
+    st.write("Distribuição por Grau de Instrução")
+
+    base_filtrada = base[
+        (base['NM_UE'] == unidade_selecionada) & (base['DS_CARGO'] == cargo_selecionado)
+    ]
+    grau_instrucao = base_filtrada['DS_GRAU_INSTRUCAO'].value_counts().reset_index()
+    grau_instrucao.columns = ['DS_GRAU_INSTRUCAO', 'Quantidade']
+
+    st.bar_chart(grau_instrucao.set_index('DS_GRAU_INSTRUCAO')['Quantidade'], x_label='DS_GRAU_INSTRUCAO', y_label='COUNT')
+
+    st.header("Relação entre Gênero e Grau de Instrução")
+
+    st.write("Grau de Instrução por Gênero")
+
+    st.bar_chart(grau_instrucao.set_index('DS_GRAU_INSTRUCAO')['Quantidade'], x_label='DS_GRAU_INSTRUCAO', y_label='COUNT', color=base('DS_GENERO'), stack=False)
