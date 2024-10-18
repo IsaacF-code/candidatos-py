0bbd27c4a2b67de0c358a7333aaf807f126953e3
Isaac Freires Santos <isaacfreires@gmail.com>
2024-10-18 12:01:52 -0300

    ✨ feat: Adicionado Filtros de Unidade eleitoral e Cargos

--- candidatos.py
+++ candidatos.py
@@ -3,20 +3,38 @@ import pandas as pd
 from io import StringIO
 import plotly.express as px
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    # bytes_data = uploaded_file.getvalue()
-    # st.write(bytes_data)
+base_csv = st.file_uploader("Escolha o arquivo CSV com os dados dos candidatos")
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+if base_csv != None:
+    bytes_data = base_csv.getvalue()
+    string_data = StringIO(bytes_data.decode("ISO-8859-1"))
+    base = pd.read_csv(string_data, sep=';', decimal=',')
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+    st.header("Prévia dos Dados")
+    base
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+st.sidebar.header("Filtros")
+
+unidade_eleitoral = base.groupby('NM_UE')
+
+st.sidebar.selectbox(
+    "Selecione uma unidade eleitoral(NM_UE)",
+    unidade_eleitoral,
+    index=None
+)
+
+cargo = base.groupby('DS_CARGO')
+
+st.sidebar.selectbox(
+    "Selecione um Cargo (DS_CARGO)",
+    cargo,
+    index=None
+)
+
+st.header("Distribuição do Grau de Instrução")
+
+st.write("Distribuição por Grau de Instrução")
+
+grau_instrucao = base.groupby('DS_GRAU_INSTRUCAO').agg({'count': 'sum'}).reset_index()
+
+st.bar_chart(grau_instrucao, x='DS_GRAU_INSTRUCAO', y='count')
